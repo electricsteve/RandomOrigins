aeb32529afcf9928ac63b5427a544754499baaa9
ElectricSteve <nathanndees@gmail.com>
2023-11-26 16:18:33 +0100

    Removed model, just basic arrow now

--- src/main/java/electricsteve/random_origins/RandomOriginsClient.java
+++ src/main/java/electricsteve/random_origins/RandomOriginsClient.java
@@ -1,11 +1,9 @@
 package electricsteve.random_origins;
 
-import electricsteve.random_origins.client.model.entities.ThunderArrowEntityModel;
 import electricsteve.random_origins.client.render.entities.ThunderArrowEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.util.Identifier;
@@ -19,7 +17,5 @@ public class RandomOriginsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EntityRendererRegistry.register(RandomOrigins.THUNDER_ARROW, ThunderArrowEntityRenderer::new);
-
-        EntityModelLayerRegistry.registerModelLayer(MODEL_THUNDER_ARROW_LAYER, ThunderArrowEntityModel::getTexturedModelData);
     }
 }
--- src/main/java/electricsteve/random_origins/client/render/entities/ThunderArrowEntityRenderer.java
+++ src/main/java/electricsteve/random_origins/client/render/entities/ThunderArrowEntityRenderer.java
@@ -1,8 +1,6 @@
 package electricsteve.random_origins.client.render.entities;
 
 import electricsteve.random_origins.Entities.ThunderArrowEntity;
-import electricsteve.random_origins.RandomOriginsClient;
-import electricsteve.random_origins.client.model.entities.ThunderArrowEntityModel;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.render.entity.EntityRendererFactory;
@@ -12,11 +10,9 @@ import net.minecraft.util.Identifier;
 @Environment(EnvType.CLIENT)
 public class ThunderArrowEntityRenderer extends ProjectileEntityRenderer<ThunderArrowEntity> {
     public static final Identifier TEXTURE = new Identifier("randomorigins", "textures/entity/thunder_arrow.png");
-    protected ThunderArrowEntityModel model;
 
     public ThunderArrowEntityRenderer(EntityRendererFactory.Context ctx) {
         super(ctx);
-        this.model = new ThunderArrowEntityModel(ctx.getPart(RandomOriginsClient.MODEL_THUNDER_ARROW_LAYER));
     }
 
     @Override
